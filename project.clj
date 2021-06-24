09c69793ff44116fd07c2253a3e1189ca98355a4
Thomas Crowley <git@tscrowley.dev>
2021-06-24 15:01:32 +1000

    Fix classifier name

--- project.clj
+++ project.clj
@@ -28,9 +28,9 @@
                  [org.apache.kafka/kafka-streams "2.8.0" :scope "provided" :exclusions [com.fasterxml.jackson.core/jackson-core]]]
   :uberjar {:prep-tasks ["clean" "javac" "compile"]
             :aot        :all}
-  :classifiers [["source" {:source-paths      ^:replace []
-                           :java-source-paths ^:replace ["src/java"]
-                           :resource-paths    ^:replace ["javadoc"]}]
+  :classifiers [["sources" {:source-paths      ^:replace []
+                            :java-source-paths ^:replace ["src/java"]
+                            :resource-paths    ^:replace ["javadoc"]}]
                 ["javadoc" {:source-paths      ^:replace []
                             :java-source-paths ^:replace []
                             :resource-paths    ^:replace ["javadoc"]}]]
