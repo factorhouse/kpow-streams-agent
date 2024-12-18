480b5da2e6dc7147768f61413cb53589d58a3948
Thomas Crowley <tom@factorhouse.io>
2024-12-18 15:29:00 +1100

    Flesh out static MetricFilter methods

--- src/java/io/factorhouse/kpow/MetricFilter.java
+++ src/java/io/factorhouse/kpow/MetricFilter.java
@@ -50,10 +50,66 @@ public class MetricFilter {
         this.filterId = id;
     }
 
+    /**
+     * Returns the default metricsFilter used by the streams agent.
+     * By default, Kpow's streams agent will only send across the topology and streams state
+     * on each observation.
+     *
+     * @return the default metrics filter
+     */
     public static MetricFilter defaultMetricFilter() {
         return new MetricFilter("default")
-                .acceptNameStartsWith("streams.state")
                 .deny();
+    }
+
+    /**
+     * Returns a metrics filter that accepts all numeric metrics from the running Streams application.
+     *
+     * @return accept all metric filter
+     */
+    public static MetricFilter acceptAllMetricFilter() {
+        return new MetricFilter("acceptAll").accept();
+    }
+
+
+    /**
+     * Returns a metrics filter that includes only state store (and
+     *
+     * @return state store metrics only filter
+     */
+    public static MetricFilter stateStoreMetricsOnlyFilter() {
+        Predicate<MetricName> stateStoreMetricsOnly = m ->
+                m.tags().containsKey("store") ||
+                        m.tags().containsKey("in-memory-state-id") ||
+                        m.tags().containsKey("in-memory-window-state-id") ||
+                        m.tags().containsKey("in-memory-session-state-id") ||
+                        m.tags().containsKey("rocksdb-session-state-id") ||
+                        m.tags().containsKey("rocksdb-state-id") ||
+                        m.tags().containsKey("rocksdb-window-state-id");
+        return new MetricFilter("stateStoreMetricsOnly")
+                .accept(stateStoreMetricsOnly);
+    }
+
+    public static MetricFilter essentialMetricsOnlyFilter() {
+        return new MetricFilter("essentialMetricsOnlyFilter")
+                // Lagency
+                .acceptNameStartsWith("commit-latency-avg")
+                .acceptNameStartsWith("process-latency-avg")
+                .acceptNameStartsWith("poll-latency-avg")
+                // Throughput
+                .acceptNameStartsWith("process-rate")
+                .acceptNameStartsWith("records-processed-rate")
+                // Lag
+                .acceptNameStartsWith("commit-rate")
+                .acceptNameStartsWith("records-lag-max")
+                .acceptNameStartsWith("records-lag")
+                // Stability
+                .acceptNameStartsWith("failed-stream-threads")
+                .acceptNameStartsWith("rebalances")
+                // State store health
+                .acceptNameStartsWith("put-rate")
+                .acceptNameStartsWith("get-rate")
+                .acceptNameStartsWith("flush-rate");
     }
 
     public List<FilterCriteria> getFilters() {
@@ -61,7 +117,9 @@ public class MetricFilter {
     }
 
     public MetricFilter accept() {
-        Predicate<MetricName> acceptPredicate = (_filter) -> { return true; };
+        Predicate<MetricName> acceptPredicate = (_filter) -> {
+            return true;
+        };
         FilterCriteria criteria = new FilterCriteria(acceptPredicate, FilterType.ACCEPT);
         this.filters.add(criteria);
         return this;
@@ -74,7 +132,9 @@ public class MetricFilter {
     }
 
     public MetricFilter deny() {
-        Predicate<MetricName> denyFilter = (_filter) -> { return true; };
+        Predicate<MetricName> denyFilter = (_filter) -> {
+            return true;
+        };
         FilterCriteria criteria = new FilterCriteria(denyFilter, FilterType.DENY);
         this.filters.add(criteria);
         return this;
@@ -87,16 +147,21 @@ public class MetricFilter {
     }
 
     public MetricFilter acceptNameStartsWith(String prefix) {
-        Predicate<MetricName> acceptFilter = (metricName) -> { return metricName.name().startsWith(prefix); };
+        Predicate<MetricName> acceptFilter = (metricName) -> {
+            return metricName.name().startsWith(prefix);
+        };
         FilterCriteria criteria = new FilterCriteria(acceptFilter, FilterType.ACCEPT);
         this.filters.add(criteria);
         return this;
     }
 
     public MetricFilter denyNameStartsWith(String prefix) {
-        Predicate<MetricName> denyFilter = (metricName) -> { return metricName.name().startsWith(prefix); };
+        Predicate<MetricName> denyFilter = (metricName) -> {
+            return metricName.name().startsWith(prefix);
+        };
         FilterCriteria criteria = new FilterCriteria(denyFilter, FilterType.DENY);
         this.filters.add(criteria);
         return this;
     }
 }
+
