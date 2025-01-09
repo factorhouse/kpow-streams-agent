3387a4e0fe10942ad87b33d918379831a5e40b30
Thomas Crowley <tom@factorhouse.io>
2025-01-09 13:49:27 +1100

    io.factorhouse/kpow-streams-agent "1.0.0-rc9"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.factorhouse/kpow-streams-agent "1.0.0-rc8"
+(defproject io.factorhouse/kpow-streams-agent "1.0.0-rc9"
   :description "Kpow's Kafka Streams monitoring agent"
   :url "https://github.com/factorhouse/kpow-streams-agent"
   :license {:name         "Apache-2.0 License"
